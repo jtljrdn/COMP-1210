18360dd2b5dd872517158e793ce214ce19dad38c
Jordan Lee <jtlee@rmgj2.com>
2023-09-14 15:09:34 -0500

    commit

--- First/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main (String[] args){
-        int wage;
-        wage = 20;
-        System.out.print("Salary is ");
-        System.out.println(wage * 40 * 52);
-    }
-}
