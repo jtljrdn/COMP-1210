30e7e2b7f88e077ed2645bf061498cbfd03db0c8
Jordan Lee <jtlee@rmgj2.com>
2023-09-14 16:29:41 -0500

    lab 3/4

--- Lab 9-14/src/LabProgram1.java
+++ Lab 9-14/src/LabProgram1.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class LabProgram1 {
+    /*
+    Write a program with total change amount in pennies as an integer input,
+    and output the change using the fewest coins, one coin type per line.
+    The coin types are Dollars, Quarters, Dimes, Nickels, and Pennies.
+    Use singular and plural coin names as appropriate, like 1 Penny vs. 2 Pennies.
+     */
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         int totalPennies = scnr.nextInt();
--- Lab 9-14/src/LabProgram2.java
+++ Lab 9-14/src/LabProgram2.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 public class LabProgram2 {
+    /*
+    A year in the modern Gregorian Calendar consists of 365 days.
+    In reality, the earth takes longer to rotate around the sun.
+    To account for the difference in time, every 4 years, a leap year takes place.
+    A leap year is when a year has 366 days: An extra day, February 29th.
+    The requirements for a given year to be a leap year are:
+    1) The year must be divisible by 4
+    2) If the year is a century year (1700, 1800, etc.), the year must be evenly divisible by 400; therefore, both 1700 and 1800 are not leap years
+
+    Some example leap years are 1600, 1712, and 2016.
+    Write a program that takes in a year and determines whether that year is a leap year.
+     */
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         int inputYear;
