b1df7e9ef06c6f3467420384c3e035e3faee4fe7
Jordan Lee <jtlee@rmgj2.com>
2023-09-14 15:26:53 -0500

    commit

--- Lab 9-13/src/LabProgram1.java
+++ Lab 9-13/src/LabProgram1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LabProgram {
+public class LabProgram1 {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         String inputMonth = scnr.next();
